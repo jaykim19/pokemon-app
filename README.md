17e604b23d7e37efe7520cf892f61cb7506e359e
jaykim19 <nobletime@naver.com>
2023-07-29 22:47:22 +0900

    포켓몬을 카드타입으로 정렬하기 위해 pokemon-card 컴포넌트 생성, pokemon-grid 컴포넌트 수정

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-This is Pokemon API App.
+This is Pokemon API App. I used the nuxtjs app structure (npx create-nuxt-app 'project name').
 
 Remove unnecessary files and move to <main> tag to layout.tsx.
 <main> tag wrap around <div> tag and children component, so the title should be fixed whatever children components comes up.
+
+Created pokemon-grid.tsx to make a grid for the listing of the pokemons.
+pokemon-grid.tsx contains <input> tag to type the text.
+
+Created pokemon-card.tsx to show the pokemons by card types.
+Created interface to transfer the data by props
---
+++ components/pokemon-grid.tsx
@@ -0,0 +1,27 @@
+'use client'
+// should be used because of useState
+
+import { useState } from "react"
+
+export function PokemonGrid() {
+    const [text, setText] = useState("")
+
+    return (
+        <>
+            <div>
+                <h3 className="text-2xl py-6 text-center">Search Your Pokemon</h3>
+                <div className="grid w-full max-w-sm items-center gap-1.5"></div>
+                <input 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    type="text" 
+                    value={text} 
+                    id="pokemonName" 
+                    autoComplete="off" 
+                    placeholder="Enter Your Pokemon Name..." 
+                    onChange={(event) => setText(event.target.value)} 
+                />
+            </div>
+            <h3 className="text-4xl pt-12 pb-6 text-center">Pokemons</h3>
+        </>
+    )
+}
