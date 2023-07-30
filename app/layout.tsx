2391e6100047266701c106587ee5fd70e3fe3fc5
jaykim19 <nobletime@naver.com>
2023-07-30 11:59:09 +0900

    favicon 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import pocketball from '@/public/poketball.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Pokemon App',
-  description: 'Pokemon Search App',
+  description: 'Pokemon App',
 }
 
 export default function RootLayout({
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/favicon.ico' />
+      </head>
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center p-24">
           <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
